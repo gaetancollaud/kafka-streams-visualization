44a55098ceed6405e142cc3d6cdb322346ab2c07
Gaétan Collaud <gaetancollaud@gmail.com>
2021-11-24 21:15:51 +0100

    fix naming

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/kafka-stream-mermaid'),
+      dir: require('path').join(__dirname, './coverage/kafka-stream-visualization'),
       subdir: '.',
       reporters: [
         { type: 'html' },
